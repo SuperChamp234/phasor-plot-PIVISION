c0dd6d577ff25cb94c993f919b4656e8d2815aa9
SuperChamp234 <leonardh@tuta.io>
2022-03-04 10:44:09 +0530

    commit

--- sym-phasorplot.js
+++ sym-phasorplot.js
@@ -6,12 +6,25 @@
         this.onDataUpdate = dataUpdate;
 
         function dataUpdate(data) {
-            if(data) {
+            var test_data = {
+                vector1_mag: 50,
+                vector1_angle: 60,
+            }
+            if(test_data) {
                 scope.h = 150;
                 scope.w = 150;
                 scope.cx= 150/2;
-                scope.cy = 150/2
+                scope.cy = 150/2;
+                scope.circleR = vector1_mag;
+                [scope.l1ex2, scope.l1ey2] = drawLine(scope, test_data.vector1_mag, vector1_angle);
             }
+        }
+        function drawLine(scop, vectorMag, vectorAng){
+            var x1 = scope.cx;
+            var y1 = scope.cy;
+            var x2 = scope.cx + vectorMag * Math.cos(vectorAng);
+            var y2 = scope.cy + vectorMag * Math.sin(vectorAng);
+            return [x2,y2];
         }
     };
 
