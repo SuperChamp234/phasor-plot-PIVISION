eb78423cf3aa1dbc237eb34f54567d9f7854c5b5
SuperChamp234 <leonardh@tuta.io>
2022-03-11 14:49:31 +0530

    commit

--- sym-phasorplot.js
+++ sym-phasorplot.js
@@ -3,30 +3,63 @@
     CS.deriveVisualizationFromBase(phasorplot);
 
 	phasorplot.prototype.init = function (scope) {
+        let state = {
+            h: 150,
+            w: 150,
+            cx: 150/2,
+            cy: 150/2,
+            circleR: 50,
+            IAang: 0,
+            IBang: 0,
+            ICang: 0,
+            IAmag: 50,
+            IBmag: 50,
+            ICmag: 50,
+            showIA: false,
+            showIB: false,
+            showIC: false,
+        }
         this.onDataUpdate = dataUpdate;
 
         function dataUpdate(data) {
             if(data) {
                 console.log(data)
-                //test data
-                let vectorMag=50;
-                let vectorAng=60;
-                //
-                scope.h = 150;
-                scope.w = 150;
-                scope.cx= 150/2;
-                scope.cy = 150/2;
-                scope.circleR = vectorMag;
-                // console.log(l1ex2);
-                // console.log(l1ey2);
-                [scope.l1ex2, scope.l1ey2] = drawLine(scope, vectorMag, parseInt(data.Value))
+                scope = state;
+                handleInput(data.Label,data.Value,state);
+                draw(state);
             }
         }
-        function drawLine(scope, vectorMag, vectorAng){
+        function draw(state,scope){
+
+            if (state.showIA) {
+                [scope.lAex2, scope.lAey2] = plotLine(state, state.IAmag, parseInt(state.IAang))
+            }
+            if (state.showIB) {
+                [scope.lBex2, scope.lBey2] = plotLine(state, state.IBmag, parseInt(state.IBang))
+            }
+            if (state.showIC) {
+                [scope.lCex2, scope.lCey2] = plotLine(state, state.ICmag, parseInt(state.ICang))
+            }
+        }
+        function plotLine(state, vectorMag, vectorAng){
             vectorAng = -3.14*(vectorAng/180);
-            var x2 = scope.cx + vectorMag * Math.cos(vectorAng);
-            var y2 = scope.cy + vectorMag * Math.sin(vectorAng);
+            var x2 = state.cx + vectorMag * Math.cos(vectorAng);
+            var y2 = state.cy + vectorMag * Math.sin(vectorAng);
             return [x2,y2];
+        }
+        function handleInput(Label, Value,state){
+            LabelSplit = Label.split("|");
+            if(LabelSplit[1] == "IAangle"){
+                state.IAang = Value;
+                state.showIA = true;
+            } else if(LabelSplit[1] == "IBangle"){
+                scope.IBang = Value;
+                state.showIB = true;
+            }
+            else if(LabelSplit[1] == "ICangle"){
+                scope.ICang = Value;
+                state.showIC = true;
+            }
         }
     };
 
